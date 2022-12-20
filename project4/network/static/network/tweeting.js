f95b647a448c871dcace60963e8df05d1a7ed599
John Zolton <jgz6@uakron.edu>
2022-12-20 09:34:29 -0500

    enabled edit button

--- project4/network/static/network/tweeting.js
+++ project4/network/static/network/tweeting.js
@@ -33,6 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     document.querySelector(`#${this.id}`).innerText = 'Liked';
                     this.dataset.type = 'unlike'
                   })}
+
             if (this.dataset.type === 'unlike') {
                 fetch('unlike', {
                     method: 'POST',
@@ -59,12 +60,40 @@ document.addEventListener('DOMContentLoaded', function() {
                     this.dataset.type = 'like'
                     }) 
             }
-        if (this.dataset.type === 'edit'){
-            console.log('edit button clicked!')
-        }
- 
 
-
+            if (this.dataset.type === 'save'){
+                let area_address = `#text-${this.id}`
+                console.log(area_address)
+                let tweet_body = document.querySelector(area_address).innerHTML;
+                console.log(tweet_body)
+                console.log('savebutton hit!')
+                /*
+                fetch('update',{
+                    method:'POST',
+                    body: JSON.stringify({
+                        tweet_id: twt,
+                        body: this.dataset.likes})
+                    })
+                    .then(response => response.json())
+                    .then(result => {
+                        // Print result
+                        console.log(result);
+                        console.log(result['likes'])
+                        let like_count = parseInt(result['likes'])
+                        console.log(like_count)
+                })
+                */
+               this.dataset.type = 'edit';
+               document.querySelector(`#${this.id}`).innerText = 'Edit';
+            }
+            if (this.dataset.type === 'edit'){
+                console.log(this.body)
+                let address = `#body-${this.dataset.id}`
+                let tweet_body = document.querySelector(address).innerHTML;
+                this.dataset.type = 'save'
+                document.querySelector(`#${this.id}`).innerText = 'Save';
+                document.querySelector(address).innerHTML=  `<textarea id='text-${this.id}' maxlength="140" cols= "49" rows="4">${tweet_body}</textarea>`;
+            }
         }
     });
  });
